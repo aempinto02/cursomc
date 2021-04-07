425a457bd247f602abcf9f961c06172ae7d96f72
André Pinto <aempinto02@gmail.com>
2021-04-07 15:21:11 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/ston/cursomc/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.ston.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ston.cursomc.domain.Pedido;
+import com.ston.cursomc.repositories.PedidoRepository;
+import com.ston.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repository;
+
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				 "Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName())); 
+	}
+}
