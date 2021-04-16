dd3a9865055b9fc1f8d093d8064d8db39a1ece30
André Pinto <aempinto02@gmail.com>
2021-04-16 13:05:58 -0300

    Enviando imagem via endpoint

--- src/main/java/com/ston/cursomc/services/ClienteService.java
+++ src/main/java/com/ston/cursomc/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.ston.cursomc.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,6 +12,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.ston.cursomc.domain.Cidade;
 import com.ston.cursomc.domain.Cliente;
@@ -37,6 +39,9 @@ public class ClienteService {
 
 	@Autowired
 	private EnderecoRepository endRepository;
+	
+	@Autowired
+	private S3Service s3Service;
 
 	public Cliente find(Integer id) {
 		
@@ -102,5 +107,9 @@ public class ClienteService {
 	private void updateData(Cliente newCliente, Cliente cliente) {
 		newCliente.setNome(cliente.getNome());
 		newCliente.setEmail(cliente.getEmail());
+	}
+	
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return s3Service.uploadFile(multipartFile);
 	}
 }
