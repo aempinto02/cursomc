ab2e1601dd261524a0db950f4dd06781a5182dbf
André Pinto <aempinto02@gmail.com>
2021-04-16 14:45:19 -0300

    Salvando a URL da imagem em Cliente

--- src/main/java/com/ston/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/ston/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.model.AmazonS3Exception;
 import com.ston.cursomc.services.exceptions.AuthorizationException;
 import com.ston.cursomc.services.exceptions.DataIntegrityException;
@@ -60,9 +61,17 @@ public class ResourceExceptionHandler {
 	}
 
 	@ExceptionHandler(AmazonS3Exception.class)
-	public ResponseEntity<StandardError> amazonService(AmazonS3Exception e, HttpServletRequest request) {
+	public ResponseEntity<StandardError> amazonS3(AmazonS3Exception e, HttpServletRequest request) {
 		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+	
+	@ExceptionHandler(AmazonServiceException.class)
+	public ResponseEntity<StandardError> amazonService(AmazonServiceException e, HttpServletRequest request) {
+		
+		HttpStatus code = HttpStatus.valueOf(e.getErrorCode());
+		StandardError err = new StandardError(code.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(code).body(err);
 	}
 	
 	@ExceptionHandler(AmazonClientException.class)
